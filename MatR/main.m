8c150317381a042b2fb9dfb01f19d7c910b0982d
davidcox95 <davidcox95@comcast.net>
2014-06-11 19:47:23 -0400

    First major commit

---
+++ MatR/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MatR
+//
+//  Created by David Cox on 6/11/14.
+//  Copyright (c) 2014 David Cox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
