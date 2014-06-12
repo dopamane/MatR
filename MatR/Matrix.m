eefa37eea40f711f15e5a9cfdc18e8a8c4bc19e7
davidcox95 <davidcox95@comcast.net>
2014-06-11 20:00:19 -0400

    Removed unused variable

--- MatR/Matrix.m
+++ MatR/Matrix.m
@@ -92,7 +92,7 @@
     if (index <= rows)
     {
         Matrix *temp_mat = [[Matrix alloc] initValue: (rows+1) by: columns value:0];
-        for (int r = 0, t = 0; r < rows+1; r++)
+        for (int r = 0; r < rows+1; r++)
         {
             for (int c = 0; c < columns; c++)
             {
@@ -297,80 +297,4 @@
 
 @end
 
-int main(int argc, const char * argv[])
-{
-    
-    @autoreleasepool {
-        
-        int rows = 5, cols = 4;
-        NSMutableArray *m1 = [[ NSMutableArray alloc] initWithCapacity: rows];
-        NSMutableArray *row1 = [[NSMutableArray alloc] initWithCapacity: cols];
-        
-        for (int c = 0; c < cols; c++)
-            [row1 addObject: [NSNumber numberWithInt: c+1]];
-        
-        for (int i = 0; i < rows; i++)
-            [m1 addObject: row1];
-        
-        Matrix *matrix1 = [[Matrix alloc] initWith: rows by: cols with: m1];
-        [matrix1 print];
-        
-        NSLog(@"\n");
-        
-        NSMutableArray *m2 = [[NSMutableArray alloc] initWithCapacity: rows];
-        NSMutableArray *row2 = [[NSMutableArray alloc] initWithCapacity: cols];
-        for (int c = 0; c < cols; c++)
-            [row2 addObject: [NSNumber numberWithInt: c+2]];
-        for (int i = 0; i < rows; i++)
-            [m2 addObject: row2];
-        
-        Matrix *matrix2 = [[Matrix alloc] initWith:rows by:cols with:m2];
-        [[[matrix2 matrix] objectAtIndex:1] replaceObjectAtIndex:2 withObject:[NSNumber numberWithDouble:16]];
-        [matrix2 print];
-        
-        NSLog(@"\n");
-        
-        Matrix *matrix3 = [matrix2 transpose];
-        [matrix3 print];
-        
-        NSLog(@"\n");
-        
-        Matrix *matrix4 = [[Matrix alloc] initValue: 3 by: 2 value:[NSNumber numberWithInteger:2]];
-        [matrix4 print];
-        NSLog(@"\n");
-        
-        Matrix *matrix5 = [[Matrix alloc] initValue: 2 by: 4 value:[NSNumber numberWithInteger:4]];
-        [matrix5 print];
-        NSLog(@"\n");
-        
-        Matrix *matrix6 = [matrix4 multiplyMatrix:matrix5];
-        [matrix6 print];
-        NSLog(@"\n");
-        
-        NSMutableArray *row = [NSMutableArray arrayWithCapacity:4];
-        [row addObject:[NSNumber numberWithDouble:3]];
-        [row addObject:[NSNumber numberWithDouble:4]];
-        [row addObject:[NSNumber numberWithDouble:5]];
-        [row addObject:[NSNumber numberWithDouble:6]];
-        Matrix *matrix7 = [matrix6 insertRow:row atRowIndex:1];
-        [matrix7 print];
-        
-        NSLog(@"\n");
-        
-        NSLog(@"%@\n",[matrix7 sumColumnAtIndex:0]);
-        NSLog(@"%@\n",[matrix7 sumRowAtIndex:1]);
-        NSLog(@"\n");
-        
-        [[matrix7 getColumnAtIndex:1] print];
-        
-        NSLog(@"\n");
-        
-        [[matrix7 getRowAtIndex:1] print];
-        
-        NSLog(@"\n");
-        NSLog(@"%@", [matrix7 getElementAtRow:1 andColumn:2]);
-        NSLog(@"\n");
-        [[matrix7 getSubMatrixFromRow:1 toRow:3 andFromColumn:1 toColumn:3] print];
-    }
-    return 0;
-}
+
